c0ba743ef314d7f3d31ef44831668629bb77513d
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2024-11-14 18:16:15 +0100

    Setting up main test directory

--- temperature_sensor/test/main/unit_test.c
+++ temperature_sensor/test/main/unit_test.c
@@ -1,15 +1,13 @@
 #include "unity.h"
 #include <stdio.h>
 #include <string.h>
+#include "fff.h"
 
 static void print_banner(const char* text);
 
 void app_main(void)
 {
-    print_banner("Executing one test by its name");
-    UNITY_BEGIN();
-    unity_run_test_by_name("ds18b20 test example");
-    UNITY_END();
+    
 }
 
 static void print_banner(const char* text)
