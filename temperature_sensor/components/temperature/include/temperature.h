f24e2ceff8d7db657568a1a4b9f4ff873261ecd2
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2024-12-10 09:17:28 +0100

    Modifying ds18b20 component to accept one wire pin and moving temperature logic to "temperature" component

---
+++ temperature_sensor/components/temperature/include/temperature.h
@@ -0,0 +1,10 @@
+#include "mqtt_client.h"
+
+#define I2C_SLAVE_ADDR 0x65
+#define ONE_WIRE_PIN GPIO_NUM_16
+#define SENSOR_READ_INTERVAL_MS 5000 // Read every 5 seconds
+#define TEMP_TAG "Temperature"
+
+static float read_temperature(void);
+static void temperature_publish_task(void *arg);
+esp_err_t temperature_init(esp_mqtt_client_handle_t client);
