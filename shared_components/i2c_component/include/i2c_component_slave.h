a085df2b23f0b8d0ebd1eddb4e91be9f599faa7b
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2024-12-10 09:17:58 +0100

    Removing I2C Slave Address from i2c_component_slave.h

--- shared_components/i2c_component/include/i2c_component_slave.h
+++ shared_components/i2c_component/include/i2c_component_slave.h
@@ -9,7 +9,6 @@
 #define DATA_LENGTH 32
 #define I2C_SLAVE_SCL_IO GPIO_NUM_22
 #define I2C_SLAVE_SDA_IO GPIO_NUM_21
-#define I2C_SLAVE_ADDR 0x65
 #define TEST_I2C_PORT I2C_NUM_0
 #define TAG "I2C Slave Component"
 
